f37833034340601b50c149fd433567b046aef16b
Chaoran Lu <andyluloveusc@gmail.com>
2025-05-18 23:00:15 -0700

    use negative testing for schema verification and business logic check

--- specs/brands.spec.ts
+++ specs/brands.spec.ts
@@ -3,7 +3,6 @@ import * as supertest from "supertest";
 const baseURL = "https://practice-react.sdetunicorns.com/api/test";
 
 describe("Brands", () => {
-
   it("GET /brands", async () => {
     const res = await supertest(baseURL).get("/brands");
     expect(res.statusCode).toBe(200);
@@ -11,17 +10,83 @@ describe("Brands", () => {
     expect(Object.keys(res.body[0])).toEqual(["_id", "name"]);
   });
 
-  it.only('Schema Verification - Name is a mandatory field', async () => {
-    const wrongFormattedData = {
-        'name': '',
-        'description': 'Test Brand Description'
-    }
-    const res = await supertest(baseURL)
-        .post('/brands')
-        .send(wrongFormattedData)
-    
-    expect(res.statusCode).toEqual(422)
-    expect(res.body.error).toEqual('Name is required')
+  describe("Schema Verification", () => {
+    it("Schema Verification - Name is a mandatory field", async () => {
+      const emptyNameFieldData = {
+        name: "",
+        description: "Test Brand Description",
+      };
+      const res = await supertest(baseURL)
+        .post("/brands")
+        .send(emptyNameFieldData);
+
+      expect(res.statusCode).toEqual(422);
+      expect(res.body.error).toEqual("Name is required");
+    });
+
+    it("Schema Verifiaction - Max char length for name = 30", async () => {
+        const data = {
+            "name": "012345678901234567890123456789x"
+        }
+
+        const res = await supertest(baseURL).post('/brands').send(data)
+
+        expect(res.statusCode).toEqual(422)
+        expect(res.body.error).toEqual("Brand name is too long")
+    });
+
+    it("Schema Verification - Description must be a string", async () => {
+        const data = {
+            "name": "Sample Brand",
+            "description": 123
+        }
+
+        const res = await supertest(baseURL).post('/brands').send(data)
+
+        expect(res.statusCode).toEqual(422)
+        expect(res.body.error).toEqual("Brand description must be a string")
+    });
+  });
+
+  describe("Business Logic", () => {
+    it("Business Logic - Duplicate brand entries not allowed", async () => {
+      const name = "Test Brand " + Math.floor(Math.random() * 100000);
+      const data = {
+        name: name,
+      };
+      // first request
+      const res1 = await supertest(baseURL).post("/brands").send(data);
+
+      // second request
+      const res2 = await supertest(baseURL).post("/brands").send(data);
+
+      expect(res2.statusCode).toEqual(422);
+      expect(res2.body.error).toContain("already exists");
+    });
+
+    it("Business Logic - GET /brand/invalid_id should throw 404", async () => {
+      const res = await supertest(baseURL).get(
+        "/brands/" + "682a82fd986188d4dce5cde1"
+      );
+
+      expect(res.statusCode).toEqual(404);
+      expect(res.body.error).toContain("Brand not found.");
+    });
+
+    it("Business Logic - PUT /brands/invalid_id", async () => {
+        const data = {
+            "name": "updated"
+        }
+        const res = await supertest(baseURL).put('/brands/' + 123).send(data)
+        expect(res.statusCode).toEqual(422)
+        expect(res.body.error).toContain("Unable to update brands")
+    });
+
+    it("Business Logic - DELETE /brands/invalid_id", async () => {
+        const res = await supertest(baseURL).delete('/brands/' + 123)
+        expect(res.statusCode).toEqual(422)
+        expect(res.body.error).toContain('Unable to delete brand')
+    })
   });
 
   describe("Create, Get, Update, and Delete a brand", () => {
